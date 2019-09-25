33a238bae106404933ffadb8682bb0afcce63226
Swapnil Patil <47409544+swapnilpatil94@users.noreply.github.com>
2019-09-25 21:36:14 +0530

    Add files via upload

---
+++ mail-submit.php
@@ -0,0 +1,24 @@
+<?php
+
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		
+		$receiver_mail = "ex@mail.com"; //change mail here
+		$subject = ""; //email subject 
+
+		$message = "";
+
+		foreach ($_POST as $key => $value) {
+			$message .= $key. " : ".$value;
+		}
+
+		$mail_status = mail($receiver_mail, $subject, $message);
+
+		if ($mail_status) {
+			//mail sent successfully
+			echo "Mail Sent";
+		}
+		else{
+			// mail sending fail
+			echo "Mail Sending fail!";
+		}
+	}
