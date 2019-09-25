ddc21f6751e50da34e0a47fc8940608fcba77053
Swapnil Patil <47409544+swapnilpatil94@users.noreply.github.com>
2019-09-25 23:09:04 +0530

    Update mail-submit.php

--- mail-submit.php
+++ mail-submit.php
@@ -4,21 +4,35 @@
 		
 		$receiver_mail = "swapnilp576p@gmail.com"; //change mail here
 		$subject = ""; //email subject 
+		$from = "admin@sitename.tld";
 
 		$message = "";
 
 		foreach ($_POST as $key => $value) {
-			$message .= $key. " : ".$value;
+			$message .= $key. " : ".$value."<br>";
 		}
+
+		// To send HTML mail, the Content-type header must be set
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+		
+		// Create email headers
+		$headers .= 'From: '.$from."\r\n".
+			'Reply-To: '.$from."\r\n" .
+			'X-Mailer: PHP/' . phpversion();
 
 		$mail_status = mail($receiver_mail, $subject, $message);
 
 		if ($mail_status) {
 			//mail sent successfully
-			echo "Mail Sent";
+			echo "<script> alert('Submitted') </script>";
+			header("Location : index.html");
+			die();
 		}
 		else{
 			// mail sending fail
-			echo "Mail Sending fail!";
+			echo "<script> alert('Submission Failed') </script>";
+			header("Location : index.html");
+			die();
 		}
 	}
